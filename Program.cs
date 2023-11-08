5a8629d535bdf346d23f944e3e2931645a469f74
Kesigomon <kesigomon@gmail.com>
2023-11-08 22:02:19 +0900

    First commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace acceleration_measurement
+{
+    class Program{
+        static void Main(string[] args)
+        {
+            var program = new Program();
+            program.Run().Wait();
+        }
+        private Task Run() {
+            return Task.CompletedTask;
+        }
+    }
+}
+
